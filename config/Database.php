ae61d6361a029f28da5fa5d50914cf7f495a13b6
unknown <kenzieli23@gmail.com>
2022-03-20 22:23:36 -0500

    removed database class

--- config/Database.php
+++ config/Database.php
@@ -1,21 +1,19 @@
 <?php
-    class Database{
-        $url = getenv('JAWSDB_URL');
-        $dbparts = parse_url($url);
+    $url = getenv('JAWSDB_URL');
+    $dbparts = parse_url($url);
 
-        $host = $dbparts['host'];
-        $db_name = ltrim($dbparts['path'], '/');
-        $username = $dbparts['user'];
-        $password = $dbparts['pass'];
+    $host = $dbparts['host'];
+    $db_name = ltrim($dbparts['path'], '/');
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
 
-        try {
-            $db = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    try {
+        $db = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        } catch (PDOException $e) {
-            $error_message = 'Database Error: ';
-            $error_message .= $e->getMessage();
-            echo $error_message;
-            exit('Unable to connect to the database');
-        }
-}
+    } catch (PDOException $e) {
+        $error_message = 'Database Error: ';
+        $error_message .= $e->getMessage();
+        echo $error_message;
+        exit('Unable to connect to the database');
+    }
