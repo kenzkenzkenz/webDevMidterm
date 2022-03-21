1fef7dd6976f28f4519c72b3e0dd98993657c397
unknown <kenzieli23@gmail.com>
2022-03-21 11:37:00 -0500

    in database

--- config/Database.php
+++ config/Database.php
@@ -9,18 +9,18 @@
         $password;
 
         public function __construct(){
-            $url = getenv('JAWSDB_URL');
+            $this->db = null;
+            $this->url = getenv('JAWSDB_URL');
             $dbparts = parse_url($url);
             $host = $dbparts['host'];
             $db_name = ltrim($dbparts['path'], '/');
             $username = $dbparts['user'];
             $password = $dbparts['pass'];
-
-        }
+        
     
         try {
-            $db = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->db = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password);
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
         } catch (PDOException $e) {
             $error_message = 'Database Error: ';
@@ -28,4 +28,7 @@
             echo $error_message;
             exit('Unable to connect to the database');
         }
+
+        return $this->db;
     }
+}
