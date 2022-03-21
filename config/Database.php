d252b8bad5710fedeecbf40a3f01118452008d62
unknown <kenzieli23@gmail.com>
2022-03-21 12:08:30 -0500

    used database example

--- config/Database.php
+++ config/Database.php
@@ -1,33 +1,26 @@
 <?php
     class Database{
+        //DB Params
+        $url = getenv('JAWSDB_URL');
+        $dbparts = parse_url($url);
+        $host = $dbparts['host'];
+        $db_name = ltrim($dbparts['path'], '/');
+        $username = $dbparts['user'];
+        $password = $dbparts['pass'];
 
-        $url;
-        $dbparts;
-        $host;
-        $db_name;
-        $username;
-        $password;
-
-        public function __construct(){
+        public function connect(){
             $this->db = null;
-            $this->url = getenv('JAWSDB_URL');
-            $dbparts = parse_url($url);
-            $host = $dbparts['host'];
-            $db_name = ltrim($dbparts['path'], '/');
-            $username = $dbparts['user'];
-            $password = $dbparts['pass'];
         
-    
-        try {
-            $this->db = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password);
-            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-        } catch (PDOException $e) {
-            $error_message = 'Database Error: ';
-            $error_message .= $e->getMessage();
-            echo $error_message;
-            exit('Unable to connect to the database');
-        }
+            try {
+                $this->db = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password);
+                $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+            } catch (PDOException $e) {
+                $error_message = 'Database Error: ';
+                $error_message .= $e->getMessage();
+                echo $error_message;
+                exit('Unable to connect to the database');
+            }
 
         return $this->db;
     }
