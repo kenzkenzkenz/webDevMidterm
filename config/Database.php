32d1c3d78dae1e78d010097c520f877649c61055
unknown <kenzieli23@gmail.com>
2022-03-21 13:22:58 -0500

    using conn

--- config/Database.php
+++ config/Database.php
@@ -1,30 +1,28 @@
 <?php
-    class Database{
-        //DB Params
+    class Database {
+        //Database Parameters
         $url = getenv('JAWSDB_URL');
         $dbparts = parse_url($url);
-
+    
         $host = $dbparts['host'];
         $db_name = ltrim($dbparts['path'], '/');
         $username = $dbparts['user'];
         $password = $dbparts['pass'];
+        private $conn;
 
-        private $db;
-
-        public function __construct(){
-            $this->db = null;
-        }
-
+        //Database Connect
         public function connect(){
-        
+            $this->conn = null;
+
             try {
-                $this->db = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password);
-                $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-            } catch (PDOException $error) {
-                echo "Connection failed:" . $error->$getMessage();
+                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . 
+                $this->db_name, $this->username, $this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            }catch(PDOException $e){
+                echo 'Connection Error: ' . $e->getMessage();
             }
 
-        return $this->db;
+            return $this->conn;
+        }
     }
-}
