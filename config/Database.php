1aef969af605467b8d6fe7335012a39fd3120f60
unknown <kenzieli23@gmail.com>
2022-03-21 13:01:43 -0500

    changed catch and added construct

--- config/Database.php
+++ config/Database.php
@@ -3,24 +3,26 @@
         //DB Params
         $url = getenv('JAWSDB_URL');
         $dbparts = parse_url($url);
+
         $host = $dbparts['host'];
         $db_name = ltrim($dbparts['path'], '/');
         $username = $dbparts['user'];
         $password = $dbparts['pass'];
+
         private $db;
 
-        public function connect(){
+        public function __construct(){
             $this->db = null;
+        }
+
+        public function connect(){
         
             try {
                 $this->db = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password);
                 $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         
-            } catch (PDOException $e) {
-                $error_message = 'Database Error: ';
-                $error_message .= $e->getMessage();
-                echo $error_message;
-                exit('Unable to connect to the database');
+            } catch (PDOException $error) {
+                echo "Connection failed:" . $error->$getMessage();
             }
 
         return $this->db;
