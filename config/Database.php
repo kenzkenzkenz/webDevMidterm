e86086fa60c3cb86be99b4bcede28b27e5309044
unknown <kenzieli23@gmail.com>
2022-03-21 13:28:48 -0500

    more database alterations

--- config/Database.php
+++ config/Database.php
@@ -1,14 +1,17 @@
 <?php
     class Database {
         //Database Parameters
-        $url = getenv('JAWSDB_URL');
+        private $conn;
+        private $url;
+
         $dbparts = parse_url($url);
-    
+        $this->url = getenv('JAWSDB_URL');
+        $dbparts = parse_url($this->url);
+        
         $host = $dbparts['host'];
         $db_name = ltrim($dbparts['path'], '/');
         $username = $dbparts['user'];
         $password = $dbparts['pass'];
-        private $conn;
 
         //Database Connect
         public function connect(){
