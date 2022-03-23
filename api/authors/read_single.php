a06b36413f5a6c542d3cefe7f2eff64dd1204d3e
unknown <kenzieli23@gmail.com>
2022-03-23 18:28:40 -0500

    changed authors/read_single.php  line 19

--- api/authors/read_single.php
+++ api/authors/read_single.php
@@ -16,7 +16,7 @@
     $author = new Author($db);
 
     // Get ID
-    $author->id = isset($_GET['id']) ? $_GET['id'] : die();
+    $author->id = isset($_GET['authorId']) ? $_GET['authorId'] : die();
 
     //Get author
     $author->read_single();
