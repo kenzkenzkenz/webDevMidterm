3e1da67b21cf54812a5237f44b837d2980b849d3
unknown <kenzieli23@gmail.com>
2022-03-23 23:05:45 -0500

    e

--- api/quotes/read_author.php
+++ api/quotes/read_author.php
@@ -18,7 +18,7 @@
     //Instantiate quote object
     $quote = new Quote($db);
 
-    $quote->id = isset($_GET['id']) ? $_GET['id'] : die(); //added
+    $quote->author = isset($_GET['id']) ? $_GET['id'] : die(); //added
 
     //Quote query
     $result = $quote->readQuotesByAuthorId(); //changed from read() to new function
@@ -45,7 +45,7 @@
         }
 
         if($quote->id !== null){
-            print_r(json_encode($quote_item));
+            print_r(json_encode($quotes_arr));
         } else {
             // No quotes
             echo json_encode(
