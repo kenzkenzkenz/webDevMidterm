25b63e956b15d966c2ad0627e260726ae624b5ae
unknown <kenzieli23@gmail.com>
2022-03-23 19:34:47 -0500

    a

--- api/quotes/read_combo.php
+++ api/quotes/read_combo.php
@@ -48,9 +48,11 @@
 
         //Convert to JSON and output
         echo json_encode($quotes_arr);
-    } else {
-        // No quotes
-        echo json_encode(
-            array('message' => 'No Quotes Found')
-        );
     }
+    
+    // else {
+    //     // No quotes
+    //     echo json_encode(
+    //         array('message' => 'No Quotes Found')
+    //     );
+    // }
